ba49fb59b622f63d6e8d970b051e13f50aae349c
lordhiraishin <lemardeley.quentin.pro@hotmail.fr>
2021-10-19 14:57:08 +0200

    avancé scénario

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -9,114 +9,125 @@ import time
 
 GoodAnswer = True
 nb = 1
-# chanceSinge = 30
-# chanceChefSinge = 70
-# chancePlayer = 80
 nbSinge = 1
 Fizz = "Fizz !"
 Buzz = "Buzz !"
 FizzBuzz = "FizzBuzz !"
 
-# GameStart = True
+
 
 
 
 # Functions
+
+print("En explorant la jungle, tu remarques une bande de singes braillards évoluant dans les arbres. En écoutant avec un  peu plus d’attention, tu te rends comptes que les singes semblent articuler des nombres et que deux sons  reviennent régulièrement : Fizz et Buzz… ")
+print("À un moment, les cris s’arrêtent et tu te retrouves subitement entouré par les singes.")
+print("L’un d’eux, probablement le chef vu sa démarche assurée, descend de l’arbre et s’approche de toi. Il te toise un  instant puis, du doigt, te désigne un endroit en hauteur. En levant la tête, tu aperçois une grosse clé en or, pendue  à une solide liane, tu commences à chercher un moyen de grimper dans les arbres, mais cela déclenche une vive  réaction de la part des singes. Il semble qu’ils ne soient pas disposés à te donner la clé comme ça !")
+print("Tu te ravises alors, et le chef se rapproche de toi et commence à te parler…")
+print("« Toi jouer ! Si gagner alors clé à toi ! »")
+print("Jouer ? Mais à quoi ? ")
+print("Semblant lire dans tes pensées, le chef dit alors « Nous montrer. » Puis il dit « 1 ». Un autre singe dans les arbres  dit alors « 2 », puis un troisième « Fizz » et ainsi de suite : « 4 », « Buzz », « Fizz », « 7 », « 8 », « Fizz », « Buzz »,  « 11 », « 12 »")
+print(" À ce moment, tous les autres singes se mettent à rire et celui qui vient d’annoncer 12 pousse un  cri de déception et se retourne sur sa branche en boudant, puis le jeu recommence avec les singes restants : « 1 »,  « 2 », « Fizz »…")
+print("Au bout de quelques tours tu comprends le principe du jeu et tu tentes ta chance pour obtenir la clé.")
+print()
 
 
 with open ("FizzBuzzPlayers.json", encoding= "UTF-8") as SingeFile:
         FizzbuzzPlayers = json.load(SingeFile)
 
     
-while GoodAnswer == True:
+
+
+listPlayer = []
+
+for players in range(len(FizzbuzzPlayers)):
+    listPlayer.append(FizzbuzzPlayers[players]["Name"])
+    # print(listPlayer)
     
-    listPlayer = []
 
-    for players in range(len(FizzbuzzPlayers)):
-        listPlayer.append(FizzbuzzPlayers[players]["Name"])
-        # print(listPlayer)
+FirstPlayer = random.randint(0,len(listPlayer)-1)
+print(f"Le premier joueur est {listPlayer[FirstPlayer]}")
+
+PremierChiffre = 1
+
+print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
+
+ChanceSinge = random.randint(FizzbuzzPlayers[5]["ChanceMin"],FizzbuzzPlayers[5]["ChanceMax"])
+ChanceChefSinge = random.randint(FizzbuzzPlayers[10]["ChanceMin"],FizzbuzzPlayers[10]["ChanceMax"])
+ChancePlayer = random.randint(FizzbuzzPlayers[0]["ChanceMin"],FizzbuzzPlayers[0]["ChanceMax"])
+# print(ChanceChefSinge)
+# print(ChancePlayer)
+# print(ChanceSinge)
+
+GameTurn = True
+while GameTurn == True:
+
+    if FirstPlayer >=  len(listPlayer)-1:
+        FirstPlayer = 0
+    else:
+        FirstPlayer += 1
+
+    
+
+
+
+
+    chance = random.randint(0,100)
+
+    if len(listPlayer) > 1:
+
+        if listPlayer[FirstPlayer] == FizzbuzzPlayers[10]["Name"]:
+            if chance > ChanceChefSinge:
+                print("Le chef singe à perdu !")
+                listPlayer.pop(FirstPlayer)
+            else:
+                PremierChiffre +=1
+                if PremierChiffre%3 == 0 and PremierChiffre%5 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {FizzBuzz}")
+                elif PremierChiffre%3 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {Fizz}")
+                elif PremierChiffre%5 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {Buzz}")
+                else:
+                    print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
+        elif listPlayer[FirstPlayer] == FizzbuzzPlayers[0]["Name"]:
+            if ChancePlayer < chance:
+                print(f"{listPlayer[FirstPlayer]} a perdu ! ")
+                print(f"Vous avez perdu le FizzBuzz")
+                listPlayer.pop(FirstPlayer)
+            else:
+                PremierChiffre +=1
+                if PremierChiffre%3 == 0 and PremierChiffre%5 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {FizzBuzz}")
+                elif PremierChiffre%3 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {Fizz}")
+                elif PremierChiffre%5 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {Buzz}")
+                else:
+                    print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
+        else: 
+            if ChanceSinge < chance:
+                print(f"{listPlayer[FirstPlayer]} a perdu ! ")
+                listPlayer.pop(FirstPlayer)
+            else:
+                PremierChiffre +=1
+                if PremierChiffre%3 == 0 and PremierChiffre%5 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {FizzBuzz}")
+                elif PremierChiffre%3 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {Fizz}")
+                elif PremierChiffre%5 == 0:
+                    print(f"{listPlayer[FirstPlayer]} dit : {Buzz}")
+                else:
+                    print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
+
+            # print(listPlayer)
         
 
-        FirstPlayer = random.randint(0,len(listPlayer)-1)
-        print(f"Le premier joueur est {listPlayer[FirstPlayer]}")
-
-        PremierChiffre = 1
-
-        print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
-
-        GameTurn = True
-        while GameTurn == True:
-
-            if FirstPlayer >=  len(listPlayer)-1:
-                FirstPlayer = 0
-            else:
-                FirstPlayer += 1
-
-            PremierChiffre +=1
-
-
-            ChanceSinge = random.randint(FizzbuzzPlayers[5]["ChanceMin"],FizzbuzzPlayers[5]["ChanceMax"])
-            ChanceChefSinge = random.randint(FizzbuzzPlayers[10]["ChanceMin"],FizzbuzzPlayers[10]["ChanceMax"])
-            ChancePlayer = random.randint(FizzbuzzPlayers[0]["ChanceMin"],FizzbuzzPlayers[0]["ChanceMax"])
-            # print(ChanceChefSinge)
-            # print(ChancePlayer)
-            # print(ChanceSinge)
-
-            # ChanceSinge = 30
-            # ChanceChefSinge = 70
-            # ChancePlayer = 80
-            chance = random.randint(0,100)
-
-            if len(listPlayer) > 1:
-
-                if listPlayer[FirstPlayer] == FizzbuzzPlayers[10]["Name"]:
-                    if chance > ChanceChefSinge:
-                        print("Le chef singe à perdu !")
-                        listPlayer.pop(FirstPlayer)
-                    else:
-                        if PremierChiffre%3 == 0 and PremierChiffre%5 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {FizzBuzz}")
-                        elif PremierChiffre%3 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {Fizz}")
-                        elif PremierChiffre%5 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {Buzz}")
-                        else:
-                            print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
-                elif listPlayer[FirstPlayer] == FizzbuzzPlayers[0]["Name"]:
-                    if ChancePlayer < chance:
-                        print(f"{listPlayer[FirstPlayer]} a perdu ! ")
-                        print(f"Vous avez perdu le FizzBuzz")
-                        listPlayer.pop(FirstPlayer)
-                    else:
-                        if PremierChiffre%3 == 0 and PremierChiffre%5 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {FizzBuzz}")
-                        elif PremierChiffre%3 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {Fizz}")
-                        elif PremierChiffre%5 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {Buzz}")
-                        else:
-                            print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
-                else: 
-                    if ChanceSinge < chance:
-                        print(f"{listPlayer[FirstPlayer]} a perdu ! ")
-                        listPlayer.pop(FirstPlayer)
-                    else:
-                        if PremierChiffre%3 == 0 and PremierChiffre%5 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {FizzBuzz}")
-                        elif PremierChiffre%3 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {Fizz}")
-                        elif PremierChiffre%5 == 0:
-                            print(f"{listPlayer[FirstPlayer]} dit : {Buzz}")
-                        else:
-                            print(f"{listPlayer[FirstPlayer]} dit : {PremierChiffre}")
-
-                    print(listPlayer)
+    else:
+        print(f"Le vainqueur est : {listPlayer[FirstPlayer]}")
+        GameTurn = False
+        
+        
                 
-        
-            else:
-                print(f"Le vainqueur est : {listPlayer[FirstPlayer]}")
-                break
-            break
-                    
     time.sleep(0.5)    
                   
--- Quêtes/Quete Nombre mysterieux.py
+++ Quêtes/Quete Nombre mysterieux.py
@@ -12,6 +12,9 @@ NbTrouve = 0
 Reset = False
 
 # Start the story of the quest
+
+print("En haut de la falaise, en bordure de forêt et pas très loin de la plage, tu découvres la statue d’un Sphinx avec une  grosse clé en bronze posée sur les pattes.")
+print("Lorsque tu t’en approches, les yeux de la statue s’illuminent et une voix se fait entendre :")
 print("Bonjour explorateur ! Pour ouvrir la porte de la montagne, atteindre le cœur de l’île et rejoindre tes compagnons,  tu devras tout d’abord prouver ta valeur individuelle en gagnant les 3 clés que tu obtiendras en relevant les défis appropriés. Ceci est le premier d’entre eux.")
 print()
 print("3 fois de suite, tu devras deviner le nombre que j’ai en tête, tu as 20 essais maximum pour les  trouver tous, es-tu prêt ? ")
@@ -47,42 +50,6 @@ for NbTrouve in range(0,3):
 
 
 
-            # while proposition2 != NbSecret2:
-            #     proposition2 = int(input("Devine le nombre entre 0 et 100:"))
-            #     print(NbEssai)
-            #     NbEssai = NbEssai + 1
-            #     if proposition2 < NbSecret2:
-            #         print("C'est plus!")
-            #     elif proposition2 > NbSecret2:
-            #         print("C'est moins !")
-            #     else:
-            #         print("Bravo, tu as réussi à trouver le second nombre !")
-            #         if NbEssai > 20:
-            #             print("Tu n'a pas réussi à trouver à temps. Recommence !")
-
-
-
-            #         while proposition3 != NbSecret3:
-            #             proposition3 = int(input("Devine le nombre entre 0 et 100:"))
-            #             print(NbEssai)
-            #             NbEssai = NbEssai + 1
-            #             if proposition3 < NbSecret3:
-            #                 print("C'est plus!")
-            #             elif proposition3 > NbSecret3:
-            #                 print("C'est moins !")
-            #             else:
-            #                 print("Bravo, tu as réussi à trouver le dernier nombre !")
-            #             if NbEssai > 20:
-            #                 print("Tu n'a pas réussi à trouver à temps. Recommence !")
-            #                 if NbEssai > 20:
-            #                     print("Tu n'a pas réussi à trouver à temps. Recommence !")
-
-
-
-# if NbEssai > 20:
-#     print("Tu n'a pas réussi à trouver à temps. Recommence !")
-
-# if proposition == NbSecret1 and proposition2 == NbSecret2 and proposition3 == NbSecret3:
     print("Félicitations, il ne t'a fallu que ", end="" )
     print(NbEssai, end="")
     print(" Essais. La clé est tienne")

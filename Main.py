ae539e97efcabd5559c2149601198abe78be1dc3
lordhiraishin <lemardeley.quentin.pro@hotmail.fr>
2021-10-25 13:27:42 +0200

    Assemblement en cours

--- Main.py
+++ Main.py
@@ -7,21 +7,26 @@
 # Functions
 
 
+from Fizzbuzz import FizzBuzz
+from Map1 import Drawmap
+from Player import Draw, Move
+from CodeCesar import CodeCesar
+from NombreMysterieux import NombreMysterieux
+
+
 def main():
+
+    Drawmap()
+    Draw()
+    Direction = input("Quelle direction voulez vous prendre")
+    Move(Direction)
+    # FizzBuzz()
+    # CodeCesar()
+    # NombreMysterieux()
     
-    def DrawMap():
-        def Move(Direction):
-
-
-
-# def start():
-
-#     PlayerName = input("Quel est votre nom ?")
-
-    # def DrawMap():
-    # def Move(Direction):
+   
 
 
 # Code starts here
 if __name__ == "__main__":
-    Main()
+    main()

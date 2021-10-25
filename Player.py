5f9dbeec55075dea32284a726924cd0d627c68c4
lordhiraishin <lemardeley.quentin.pro@hotmail.fr>
2021-10-25 13:28:46 +0200

    Début déplacement

--- Player.py
+++ Player.py
@@ -4,7 +4,7 @@
 import json
 import Variables as Var
 import RichConsole as RC
-import Map as Map
+import Map1 as Map
 
 # variables
 
@@ -15,42 +15,45 @@ def Move(Direction):
         Moves player in specified direction
         Direction -> str : direction to move
     """
-
+    
     print(f"Le joueur se déplace vers {Direction}")
 
     # save actual player position
     PlayerY = Var.PlayerData["Y"]
     PlayerX = Var.PlayerData["X"]
+    QuestStart = False
 
-    # calculate new player position
-    PlayerY += Var.PossibleActions[Direction]["DeltaY"]
-    PlayerX += Var.PossibleActions[Direction]["DeltaX"]
-    # if Direction == "H":
-    #     PlayerY -= 1
-    # elif Direction == "B":
-    #     PlayerY += 1
-    # elif Direction == "G":
-    #     PlayerX -= 1
-    # elif Direction == "D":
-    #     PlayerX += 1
+    while QuestStart == False:
 
-    # check if movement is valid
-    MapElementAtPlayerPosition = Var.MapData[PlayerY - 1][PlayerX - 1]
-    MapElementData = Var.MapElements[MapElementAtPlayerPosition]
-    if MapElementData["CanWalk"]:
-        Draw(True)
-        Var.PlayerData["Y"] = PlayerY
-        Var.PlayerData["X"] = PlayerX
-        Draw()
-        if MapElementAtPlayerPosition == "S":
-            Var.GameIsRunning = False
-            RC.ColorPrintAt(
-                "\nBRAVO, tu as trouvé la sortie.",
-                Y=Var.TextLine+2,
-                X=1)
-    else:
-        # obstacle
-        print(f"Aïe, un {MapElementData['Name']} !")
+        # calculate new player position
+        PlayerY += Var.PossibleActions[Direction]["DeltaY"]
+        PlayerX += Var.PossibleActions[Direction]["DeltaX"]
+        if Direction == "H":
+            PlayerY -= 1
+        elif Direction == "B":
+            PlayerY += 1
+        elif Direction == "G":
+            PlayerX -= 1
+        elif Direction == "D":
+            PlayerX += 1
+
+        # check if movement is valid
+        MapElementAtPlayerPosition = Var.MapData[PlayerY - 1][PlayerX - 1]
+        MapElementData = Var.MapElements[MapElementAtPlayerPosition]
+        if MapElementData["CanWalk"]:
+            Draw(True)
+            Var.PlayerData["Y"] = PlayerY
+            Var.PlayerData["X"] = PlayerX
+            Draw()
+            if MapElementAtPlayerPosition == "S":
+                Var.GameIsRunning = False
+                RC.ColorPrintAt(
+                    "\nBRAVO, tu as trouvé la sortie.",
+                    Y=Var.TextLine+2,
+                    X=1)
+        else:
+            # obstacle
+            print(f"Aïe, un {MapElementData['Name']} !")
 
 
 def Draw(Erase = False):
